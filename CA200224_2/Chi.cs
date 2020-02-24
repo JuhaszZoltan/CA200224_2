ceafe6471b00c8cc3e26e829ad8aa0d84f95fdce
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-02-24 20:16:09 +0100

    Add project files.

---
+++ CA200224_2/Chi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200224_2
+{
+    class Chi
+    {
+        public string Nev { get; set; }
+        public DateTime Szul { get; set; }
+        public int Suly { get; set; }
+        public bool Simi { get; set; }
+        public int Kor => DateTime.Now.Year - Szul.Year;
+        public Chi(string s)
+        {
+            var t = s.Split(';');
+            Nev = t[0];
+            Szul = DateTime.Parse(t[1]);
+            Suly = int.Parse(t[2]);
+            Simi = t[3] == "I";
+        }
+    }
+}
---
+++ CA200224_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200224_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Chi>();
+            var sr = new StreamReader(@"..\..\res\chi.txt");
+            while (!sr.EndOfStream)
+                list.Add(new Chi(sr.ReadLine()));
+            sr.Close();
+
+            //2.f
+            Console.WriteLine(list.Count);
+
+            //3.f
+            Console.WriteLine(
+                list.Count(c => c.Simi == true) 
+                / (float)list.Count * 100);
+            
+            //4.f
+            var ch = list.Where(c => c.Suly < 360 && c.Kor >= 8)
+                .SingleOrDefault();
+            Console.WriteLine(
+                ch is null 
+                ? "nincs" 
+                : $"Név: {ch.Nev}, kor: {ch.Kor}, súly: {ch.Suly}");
+            
+            //5.f
+            list.OrderByDescending(c => c.Suly)
+                .Take(5)
+                .ToList()
+                .ForEach(y => Console.WriteLine(
+                    "{0, -13} {1} gramm",
+                    y.Nev + ":", y.Suly));
+            //6.f
+            var t = list
+                .GroupBy(y => y.Szul.Year, y => y.Nev);
+            foreach (var e in t)
+                Console.WriteLine($"{e.Key} {e.Count()}");
+
+            Console.ReadKey();
+        }
+    }
+}
